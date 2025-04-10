23f151bc4ddb2336fb39c8a211f2adf3a5b68e31
repr <repr-man@noreply.codeberg.org>
2025-04-10 13:09:23 -0500

    Added UTF-8 alternative mode for Windows

--- Final Project Code/terminal.cpp
+++ Final Project Code/terminal.cpp
@@ -14,21 +14,29 @@ void Terminal::printOptions(
 }
 
 void Terminal::trimAndRecolor(std::string& str, int width) const {
-    auto last5 = str[width - 5];
-    auto last4 = str[width - 4];
-    auto last3 = str[width - 3];
-    auto last2 = str[width - 2];
-    auto last1 = str[width - 1];
-    str.resize(width - 5);
-    str.append(colorFade1);
-    str.push_back(last5);
-    str.append(colorFade2);
-    str.push_back(last4);
-    str.append(colorFade3);
-    str.push_back(last3);
-    str.append(colorFade4);
-    str.push_back(last2);
-    str.append(colorFade5);
-    str.push_back(last1);
-    str.append(reset);
+    if constexpr(isWindows) {
+        auto last3 = str[width - 3];
+        auto last2 = str[width - 2];
+        auto last1 = str[width - 1];
+        str.resize(width - 3);
+        str.append("...");
+    } else {
+        auto last5 = str[width - 5];
+        auto last4 = str[width - 4];
+        auto last3 = str[width - 3];
+        auto last2 = str[width - 2];
+        auto last1 = str[width - 1];
+        str.resize(width - 5);
+        str.append(colorFade1);
+        str.push_back(last5);
+        str.append(colorFade2);
+        str.push_back(last4);
+        str.append(colorFade3);
+        str.push_back(last3);
+        str.append(colorFade4);
+        str.push_back(last2);
+        str.append(colorFade5);
+        str.push_back(last1);
+        str.append(reset);
+    }
 }
