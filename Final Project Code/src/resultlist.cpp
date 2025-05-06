2f1232dece65f01303153a7817065709f9b644f2
repr <repr-man@noreply.codeberg.org>
2025-05-06 08:13:50 -0500

    Added removal logic for data consistency

--- Final Project Code/src/resultlist.cpp
+++ Final Project Code/src/resultlist.cpp
@@ -2,6 +2,7 @@
 #include "library.hpp"
 
 #include <cassert>
+#include <string>
 
 template <typename T> requires IsLibraryStorageType<T>
 ResultList<T>::~ResultList() {
@@ -34,8 +35,47 @@ template <typename T> requires IsLibraryStorageType<T>
 void ResultList<T>::remove(int index) {
     assert(index >= 0 && index < items.size());
 
+    if constexpr (std::is_same_v<T, InventoryItem>) {
+        if (items[index]->borrowerID != -1) {
+            auto borrower = lib.search(
+                {User::ID},
+                {std::to_string(items[index]->borrowerID)}
+            );
+            assert(borrower.size() == 1);
+            borrower[0].numCheckedOut -= 1;
+        }
+        for(HistoryItem& hist : lib.allHistory()) {
+            if(hist.name == items[index]->name) {
+                lib.remove(&hist);
+            }
+        }
+    } else if constexpr (std::is_same_v<T, User>) {
+        for(auto& inv : lib.allInventory()) {
+            if(inv.borrowerID == items[index]->id) {
+                inv.borrowerID = -1;
+            }
+        }
+        for(auto& hist : lib.allHistory()) {
+            if(hist.userID == items[index]->id) {
+                lib.remove(&hist);
+            }
+        }
+        for(auto& librarian : lib.allLibrarians()) {
+            if(librarian.id == items[index]->id) {
+                lib.remove(&librarian);
+            }
+        }
+    } else if constexpr (std::is_same_v<T, Librarian>) {
+        for(auto& user : lib.allUsers()) {
+            if(user.id == items[index]->id) {
+                lib.remove(&user);
+            }
+        }
+    }
+
     lib.remove(items[index]);
     items.erase(items.begin() + index);
+    
     modified = true;
 }
 
